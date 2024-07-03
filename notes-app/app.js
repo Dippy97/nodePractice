f11ea47c9dc51f77bc6c19c4c419fc1568f8b604
Diptamon Chakraborty <dripto.borty9799@gmail.com>
2024-07-03 10:34:39 +0530

    first session

---
+++ notes-app/app.js
@@ -0,0 +1,14 @@
+// ** file create and append
+// const fs = require('fs')
+// fs.writeFileSync('notes.txt', 'write file sync execute')
+// fs.appendFileSync('notes.txt', 'append on the same file')
+
+// **import own files and export function with return value 
+// const getNotes = require('./utils')
+// const note = getNotes('app file is calling, ')
+// console.log(note)
+
+// ** use npm package validator
+// const validator = require('validator')
+// console.log(validator.isEmail('gmail.com'))
+// console.log(validator.isURL('this.in'))
